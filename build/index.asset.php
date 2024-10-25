0da98c36096d971bf37b6b0fdd9eb151282d7778
K. Adam White <kadamwhite@users.noreply.github.com>
2024-10-25 13:36:07 +0900

    npx @wordpress/create-block

---
+++ build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '392d76d814f54cf459cf');
---
+++ build/view.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => '17debe7e4d8717f71107');

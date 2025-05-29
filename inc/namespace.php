1dfc9750253b75a8cbce87e1c5c699538ed60b8e
Alexis J. Villegas <14046668+ajvillegas@users.noreply.github.com>
2025-05-28 21:18:11 -0400

    Update function name to better reflect its function

--- inc/namespace.php
+++ inc/namespace.php
@@ -16,7 +16,7 @@ function bootstrap() : void {
 	add_action( 'add_attachment', __NAMESPACE__ . '\\schedule_file_size_check' );
 	add_action( ATTACHMENT_SIZE_CRON_ID, __NAMESPACE__ . '\\store_intermediate_file_sizes' );
 	add_action( 'rest_prepare_attachment', __NAMESPACE__ . '\\lookup_file_sizes_as_needed_during_rest_response', 10, 3 );
-	add_action( 'wp_head', __NAMESPACE__ . '\\get_performance_entries' );
+	add_action( 'wp_head', __NAMESPACE__ . '\\output_performance_entries' );
 }
 
 /**
@@ -172,7 +172,7 @@ function lookup_file_sizes_as_needed_during_rest_response( $response, $post, $re
  *
  * @return void
  */
-function get_performance_entries() {
+function output_performance_entries() {
 	// Bail if mediaWeight URL param is not set.
 	if ( ! isset( $_GET['mediaWeight'] ) ) {
 		return;
